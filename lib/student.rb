5da7ef7e9c57d5aac8bd8b1a36736eb3e0f18238
Linah Imbari <linahmuhonjaimbari@gmail.com>
2022-10-26 10:37:48 +0300

    Complete assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,24 @@
-class Student 
+require_relative './user'
+require 'pry'
+
+class Student < User
+
+    def initialize()
+        @knowledge = []
+       end  
+   
+
+    def learn(knowledge)
+       
+        @knowledge << knowledge      
+
+    end
+
+    def knowledge
+        @knowledge
+    end
+   
+# binding.pry
+
 
 end
